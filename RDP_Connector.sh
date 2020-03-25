92ea70f9ab0389fffd5ed7c7afb2d2164c66173b
Spencer Moore <Death259@gmail.com>
2020-03-25 12:48:40 -0500

    Added Loop and Remembering Entered Parameters
    Added a loop so that when a session is done it will bring you back to the screen to be able to start another session. This is mainly beneficial for when invalid information gets entered. Along with that it will now remember, in the same session, the Server, Port, Gateway and Username. So, those will not have to be re-entered.

--- RDP_Connector.sh
+++ RDP_Connector.sh
@@ -13,84 +13,86 @@ fi
 #    eval sudo $packageManager -qq install freerdp2-x11 yad
 #    eval sudo $packageManager -qq install freerdp yad
 #fi
-
-action=$(yad --center --width 400 --title "Connect via RDP" \
-    --window-icon="gtk-connect" \
-    --form \
-    --field="Server" "" \
-    --field="Port" "" \
-    --field="Remote Gateway" "" \
-    --field="Username" "" \
-    --field="Domain" "" \
-    --field="Compression Level":CBE "0!1!2" \
-    --field="BPP":CBE "32!24!16" \
-    --field="Multi Monitor":CHK true \
-    --field="Fullscreen":CHK true \
-    --field="Redirect Clipboard":CHK true \
-    --field="Show Theme":CHK true \
-    --field="Show Wallpaper":CHK true \
-    --field="Auto Reconnect":CHK true \
-    --button="gtk-connect:0" --button="gtk-close:1")
-ret=$?
-
-
-[[ $ret -eq 1 ]] && exit 0
-
-Server=$(echo $action 		| awk -F '|' '{ print $1 }')
-Port=$(echo $action 		| awk -F '|' '{ print $2 }')
-Gateway=$(echo $action 		| awk -F '|' '{ print $3 }')
-Username=$(echo $action 	| awk -F '|' '{ print $4 }')
-Domain=$(echo $action 		| awk -F '|' '{ print $5 }')
-BPP=$(echo $action 		| awk -F '|' '{ print $6 }')
-CompressionLevel=$(echo $action 		| awk -F '|' '{ print $7 }')
-MultiMonitor=$(echo $action 	| awk -F '|' '{ print $8 }')
-Fullscreen=$(echo $action 	| awk -F '|' '{ print $9 }')
-RedirectClipboard=$(echo $action 	| awk -F '|' '{ print $10 }')
-ShowTheme=$(echo $action 	| awk -F '|' '{ print $11 }')
-ShowWallpaper=$(echo $action 	| awk -F '|' '{ print $12 }')
-AutoReconnect=$(echo $action 	| awk -F '|' '{ print $13 }')
+while true
+do
+	action=$(yad --center --width 400 --title "Connect via RDP" \
+	    --window-icon="gtk-connect" \
+	    --form \
+	    --field="Server" "$Server" \
+	    --field="Port" "$Port" \
+	    --field="Remote Gateway" "$Gateway" \
+	    --field="Username" "$Username" \
+	    --field="Domain" "$Domain" \
+	    --field="Compression Level":CBE "0!1!2" \
+	    --field="BPP":CBE "32!24!16" \
+	    --field="Multi Monitor":CHK true \
+	    --field="Fullscreen":CHK true \
+	    --field="Redirect Clipboard":CHK true \
+	    --field="Show Theme":CHK true \
+	    --field="Show Wallpaper":CHK true \
+	    --field="Auto Reconnect":CHK true \
+	    --button="gtk-connect:0" --button="gtk-close:1")
+	ret=$?
 
 
-xfreerdpCommand="xfreerdp "
-if [ -n "$Server" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /v:\"$Server\""
-fi
-if [ -n "$Port" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /port:$Port"
-fi
-if [ -n "$Gateway" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /g:\"$Gateway\""
-fi
-if [ -n "$Username" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /u:\"$Username\""
-fi
-if [ -n "$Domain" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /d:\"$Domain\""
-fi
-if [ -n "$Bpp" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /bpp:$Bpp"
-fi
-if [ -n "$CompressionLevel" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /compression /compression-level:$CompressionLevel"
-fi
-if [ "$MultiMonitor" = "TRUE" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /multimon"
-fi
-if [ "$Fullscreen" = "TRUE" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /f"
-fi
-if [ "$RedirectClipboard" = "TRUE" ] ; then
-    xfreerdpCommand="$xfreerdpCommand +clipboard"
-fi
-if [ "$ShowTheme" != "TRUE" ] ; then
-    xfreerdpCommand="$xfreerdpCommand -themes"
-fi
-if [ "$ShowWallpaper"  != "TRUE" ] ; then
-    xfreerdpCommand="$xfreerdpCommand -wallpaper"
-fi
-if [ "$AutoReconnect"  = "TRUE" ] ; then
-    xfreerdpCommand="$xfreerdpCommand /auto-reconnect"
-fi
+	[[ $ret -eq 1 ]] && exit 0
 
-eval "$xfreerdpCommand"
-#xfreerdp /v:"$Server" /port:$Port /g:"$Gateway" +clipboard /multimon /u:"$Username"
+	Server=$(echo $action 		| awk -F '|' '{ print $1 }')
+	Port=$(echo $action 		| awk -F '|' '{ print $2 }')
+	Gateway=$(echo $action 		| awk -F '|' '{ print $3 }')
+	Username=$(echo $action 	| awk -F '|' '{ print $4 }')
+	Domain=$(echo $action 		| awk -F '|' '{ print $5 }')
+	BPP=$(echo $action 		| awk -F '|' '{ print $6 }')
+	CompressionLevel=$(echo $action 		| awk -F '|' '{ print $7 }')
+	MultiMonitor=$(echo $action 	| awk -F '|' '{ print $8 }')
+	Fullscreen=$(echo $action 	| awk -F '|' '{ print $9 }')
+	RedirectClipboard=$(echo $action 	| awk -F '|' '{ print $10 }')
+	ShowTheme=$(echo $action 	| awk -F '|' '{ print $11 }')
+	ShowWallpaper=$(echo $action 	| awk -F '|' '{ print $12 }')
+	AutoReconnect=$(echo $action 	| awk -F '|' '{ print $13 }')
+
+
+	xfreerdpCommand="xfreerdp "
+	if [ -n "$Server" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /v:\"$Server\""
+	fi
+	if [ -n "$Port" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /port:$Port"
+	fi
+	if [ -n "$Gateway" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /g:\"$Gateway\""
+	fi
+	if [ -n "$Username" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /u:\"$Username\""
+	fi
+	if [ -n "$Domain" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /d:\"$Domain\""
+	fi
+	if [ -n "$Bpp" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /bpp:$Bpp"
+	fi
+	if [ -n "$CompressionLevel" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /compression /compression-level:$CompressionLevel"
+	fi
+	if [ "$MultiMonitor" = "TRUE" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /multimon"
+	fi
+	if [ "$Fullscreen" = "TRUE" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /f"
+	fi
+	if [ "$RedirectClipboard" = "TRUE" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand +clipboard"
+	fi
+	if [ "$ShowTheme" != "TRUE" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand -themes"
+	fi
+	if [ "$ShowWallpaper"  != "TRUE" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand -wallpaper"
+	fi
+	if [ "$AutoReconnect"  = "TRUE" ] ; then
+	    xfreerdpCommand="$xfreerdpCommand /auto-reconnect"
+	fi
+
+	eval "$xfreerdpCommand"
+	#xfreerdp /v:"$Server" /port:$Port /g:"$Gateway" +clipboard /multimon /u:"$Username"
+done
